bdde5bcff9802b1702ce4e4783d13e23f8c2c6b9
Mark Snyder <20092441+mkwsnyder@users.noreply.github.com>
2024-10-28 16:04:05 -0600

    this is a test of the emergency broadcast system

--- apps/site/src/lib/utils/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const capitalize = (str: string) => {
-	return (str && str[0].toUpperCase() + str.slice(1).toLowerCase()) || '';
-};
-
-export const isNumber = (value: number) => {
-	return typeof value === 'number' && isFinite(value);
-};
